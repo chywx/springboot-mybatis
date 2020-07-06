643c281d5a0f78383069e9578f3b9a3c3c346ec2
陈海洋 <1559843332@qq.com>
2020-07-06 18:42:20 +0800

    接收list类型的对象

--- src/main/java/com/ocean/mvc/controller/UserController.java
+++ src/main/java/com/ocean/mvc/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.ocean.mvc.controller;
 
+import com.ocean.mvc.entity.Person;
+import com.ocean.mvc.entity.PersonModel;
 import com.ocean.mvc.entity.User;
 import com.ocean.mvc.service.UserService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
     @Autowired
     UserService userService;
 
@@ -23,6 +28,13 @@ public class UserController {
         int i = userService.insertUser(user);
         System.out.println("插入之后：" + user);
         return i;
+    }
+
+    @GetMapping("/testList")
+    public Object testList(PersonModel personModel) {
+        List<Person> persons = personModel.getPersons();
+        System.out.println(persons);
+        return persons;
     }
 
 }
