e69f43b852386d25cce427cca269569903b510c2
dylan <dylan@chendahai.cn>
2021-10-07 16:49:52 +0800

    删除不必要的import

--- src/main/java/com/ocean/config/MyWebMvcConfigurer.java
+++ src/main/java/com/ocean/config/MyWebMvcConfigurer.java
@@ -1,9 +1,6 @@
 package com.ocean.config;
 
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
