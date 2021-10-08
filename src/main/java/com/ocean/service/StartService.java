b81e2e76dd56961668eb631529bfd39a68ff106e
dahai <chenhaiyang@luckybus.me>
2021-10-08 17:00:21 +0800

    add service package

--- src/main/java/com/ocean/service/StartService.java
+++ src/main/java/com/ocean/service/StartService.java
@@ -1,4 +1,4 @@
-package com.ocean.config;
+package com.ocean.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,9 +11,9 @@ import org.springframework.stereotype.Service;
  * @date 2021年10月08日 上午 10:34
  */
 @Service
-public class StartApplication implements ApplicationListener<ContextRefreshedEvent> {
+public class StartService implements ApplicationListener<ContextRefreshedEvent> {
 
-    private Logger logger = LoggerFactory.getLogger(StartApplication.class);
+    private Logger logger = LoggerFactory.getLogger(StartService.class);
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
